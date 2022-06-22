50c6b29b28d9c325c89f667c2222af76f81e5341
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-22 08:31:17 -0700

    update

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,6 +4,11 @@ import { capitalizeFirstLetter } from '../../utils/helpers'
 function Nav(props) {
   const { categories = [], setCurrentCategory, currentCategory } = props
 
+  const handleClick = (item) => {
+    console.log(item)
+    return item
+  }
+
   return (
     <header className="flex-row px-1">
       <h2>
@@ -22,14 +27,12 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li className="mx-2">
-            <span>Contact</span>
+          <li className={'mx-2'}>
+            <span onClick={() => handleClick('Contact')}>Contact</span>
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
-              }`}
+              className={`mx-1 ${currentCategory.name === category.name}`}
               key={category.name}
             >
               <span
